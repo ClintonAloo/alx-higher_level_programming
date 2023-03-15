6ae7ec38b80e76f0f0333f2e81a5136c2a2810da
ClintonAloo <bclish.clinton4@gmail.com>
2023-03-15 10:40:17 -0700

    Return common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_1 = set()
-    set_2 = set()
-
-    common_elements = set1.intersection(set2)
-    return(common_elements)
-
+    return(set1.intersection(set2)
