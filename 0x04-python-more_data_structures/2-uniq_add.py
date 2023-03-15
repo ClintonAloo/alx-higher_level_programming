69a3963a21b1af81c78499599ba4ab62ee96dc4c
ClintonAloo <bclish.clinton4@gmail.com>
2023-03-15 12:50:46 -0700

    unique List

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()
-    else:
-        new_list = my_list.copy()
-        new_list[idx] = element
-        return new_list
+def uniq_add(my_list=[]):
+    new_list = []
+    sum = 0
+    for num in my_list:
+        if num not in new_list:
+            sum += num
+            new_list.append(num)
+    return sum
